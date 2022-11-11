ca6eb60c4dcbd171b717a3601cd8abb4ee50894f
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-11-11 22:03:23 +0200

    added filter favourite

--- controllers/contacts/getListContacts.js
+++ controllers/contacts/getListContacts.js
@@ -2,11 +2,12 @@ const { Contact } = require('../../models');
 
 const getListContacts = async (req, res) => {
   const { _id } = req.user;
-  const result = await Contact.find({ owner: _id }).populate(
-    'owner',
-    '_id email subscription',
-  );
-  console.log(result);
+  const { favorite } = req.query;
+
+  const result = await Contact.find(
+    !favorite ? { owner: _id } : { owner: _id, favorite: true },
+  ).populate('owner', '_id email subscription');
+
   res.status(200).json({
     status: 'success',
     code: 200,
