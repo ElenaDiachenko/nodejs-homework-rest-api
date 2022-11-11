ba435c8db0eace456696049011bad255a3c8579e
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-11-11 05:56:05 +0200

    added widdleware auth + /current

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { createError } = require('../utils');
+const { User } = require('../models');
+const { SECRET_KEY } = process.env;
+
+const auth = async (req, res, next) => {
+  const { authorization = '' } = req.headers;
+  const [bearer, token] = authorization.split(' ');
+
+  try {
+    if (bearer !== 'Bearer') throw createError(401, 'Not authorized');
+
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user) throw createError(401, 'Not authorized');
+
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.message === 'invalide signature') {
+      error.status = 401;
+    }
+    next(error);
+  }
+};
+
+module.exports = auth;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { users: ctrl } = require('../../controllers');
-const { validation, ctrlWrapper } = require('../../middlewares');
+const { auth, validation, ctrlWrapper } = require('../../middlewares');
 const { joiSignupSchema, joiLoginSchema } = require('../../models/user');
 
 router.post('/signup', validation(joiSignupSchema), ctrlWrapper(ctrl.signup));
 router.post('/login', validation(joiLoginSchema), ctrlWrapper(ctrl.login));
+// router.post('/login', validation(joiLoginSchema), ctrlWrapper(ctrl.login));
+router.get('/current', auth, ctrlWrapper(ctrl.getCurrent));
 
 module.exports = router;
